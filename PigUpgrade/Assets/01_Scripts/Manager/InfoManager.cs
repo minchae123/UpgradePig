46a2857f5259c2bcd727186a8d41acb27e5ba828
minchae123 <08minchae17@gmail.com>
2024-01-17 17:59:44 +0900

    start

---
+++ PigUpgrade/Assets/01_Scripts/Manager/InfoManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PigData
+{
+	public int level;
+	public int percentage;
+	public int purchase;
+	public int price;
+}
+
+[System.Serializable]
+public class PigAllData
+{
+	public PigData[] pig;
+}
+
+public class InfoManager : MonoBehaviour
+{
+	public TextAsset json;
+
+	public PigAllData data;
+
+	private void Awake()
+	{
+		print(json.text);
+		data = JsonUtility.FromJson<PigAllData>(json.text);
+	}
+
+	private void Start()
+	{
+	}
+
+}

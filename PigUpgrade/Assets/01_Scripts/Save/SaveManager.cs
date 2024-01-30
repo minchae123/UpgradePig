a88a7e2254bcdcbbff15b66c3f136cf3fa99057f
minchae123 <08minchae17@gmail.com>
2024-01-30 21:16:40 +0900

    save system

--- PigUpgrade/Assets/01_Scripts/Save/SaveManager.cs
+++ PigUpgrade/Assets/01_Scripts/Save/SaveManager.cs
@@ -1,10 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-	public void LoadGame()
+    private GameData gameData;
+    private string savePath;
+    private string saveFileName = "/SaveFile.txt";
+
+    private void Start()
+    {
+        gameData = new GameData();
+        savePath = Application.dataPath + "/SaveData/";
+
+        if (!Directory.Exists(savePath))
+        {
+            Directory.CreateDirectory(savePath);
+        }
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(gameData);
+        File.WriteAllText(savePath + saveFileName, json);
+        Debug.Log(json);
+    }
+
+
+    public void Load()
+    {
+        if (File.Exists(savePath + saveFileName))
+        {
+            string loadJson = File.ReadAllText(savePath + saveFileName);
+            gameData = JsonUtility.FromJson<GameData>(loadJson);
+            Debug.Log("로드 성공 !");
+        }
+        else
+        {
+            Debug.Log("저장 안댐 !!!");
+        }
+    }
+
+
+    public void LoadGame()
 	{
 
 	}
